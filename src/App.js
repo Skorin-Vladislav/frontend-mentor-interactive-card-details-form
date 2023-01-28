9430142767df6f9d794ba71b87f031fffe68eda0
Skorin Vladislav <43670004+Skorin-Vladislav@users.noreply.github.com>
2023-01-28 17:25:47 +0300

    input section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,64 @@
-import icon_complete from "./images/icon-complete.svg";
+// import icon_complete from "./images/icon-complete.svg";
+import bg_main_mobile from "./images/bg-main-mobile.png";
 import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <img className="background-mobile" src={bg_main_mobile} alt=""></img>
+
+      <div className="margin">
+        <div className="inputs">
+          <div className="input-wrapper">
+            <label htmlFor="name">Cardholder Name</label>
+            <input id="name" placeholder="e.g. Jane Appleseed"></input>
+          </div>
+
+          <div className="input-wrapper">
+            <label htmlFor="name">Card Number</label>
+            <input
+              id="card-number"
+              placeholder="e.g. 1234 5678 9123 0000"
+            ></input>
+          </div>
+
+          <div className="input-wrapper">
+            <div className="exp-date-wrapper">
+              <label htmlFor="exp-month">
+                Card Number
+                <div>
+                  <input
+                    id="exp-month"
+                    maxLength="2"
+                    pattern="[0-9]+"
+                    placeholder="MM"
+                  ></input>
+                  <input
+                    id="exp-year"
+                    maxLength="2"
+                    pattern="[0-9]+"
+                    placeholder="YY"
+                  ></input>
+                </div>
+              </label>
+
+              <label htmlFor="cvc">
+                CVC
+                <input
+                  id="cvc"
+                  maxLength="3"
+                  pattern="[0-9]+"
+                  placeholder="e.g. 123"
+                ></input>
+              </label>
+            </div>
+          </div>
+
+          <button className="submit">Confirm</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
